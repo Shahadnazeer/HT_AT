202ec9d85ab93138bdb296a421a0688d2ba463da
Shahad Mohamed Nazeer <shahadnazeer22@gmail.com>
2022-12-02 16:16:40 +0530

    Add project files.

---
+++ HT_AT/UnitTest1.cs
@@ -0,0 +1,44 @@
+using Newtonsoft.Json;
+using RestSharp;
+using System.Net;
+
+namespace HT_AT
+{
+    public class Tests
+    {
+        protected RestRequest restRequest;
+        protected RestClient restClient;
+        protected RestResponse restResponse;
+ 
+
+        [SetUp]
+        public async Task Setup()
+        {
+            restClient = new RestClient(@"https://jsonplaceholder.typicode.com/");
+            restRequest = new RestRequest("users",Method.Get);
+            restResponse = await restClient.ExecuteAsync(restRequest);
+        }
+
+
+        [Test]
+        public void Test1()
+        {
+            Assert.True(restResponse.StatusCode==HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.True(restResponse.ContentType == "application/json");
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var jsonobject = JsonConvert.DeserializeObject<List <string>>(restResponse.Content);
+
+            Assert.True(jsonobject.Count == 10);
+        }
+
+    }
+}
